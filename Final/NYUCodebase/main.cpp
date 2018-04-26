ee221b8c87a5560043cfb9237eb191543f256706
anthony_innamorato <ai974@nyu.edu>
2018-04-26 19:57:19 -0400

    added stars, enemy, bounds

--- Final/NYUCodebase/main.cpp
+++ Final/NYUCodebase/main.cpp
@@ -26,6 +26,7 @@ ShaderProgram textured;
 Matrix projectionMatrix;
 Matrix viewMatrix;
 bool done = false;
+int counter = 0;
 SDL_Event event;
 float lastFrameTicks = 0.0f;
 const Uint8 *keys = SDL_GetKeyboardState(NULL);
@@ -33,7 +34,7 @@ const Uint8 *keys = SDL_GetKeyboardState(NULL);
 struct Entity;
 std::vector<Entity*> entities;
 vector<Entity*> starsVec;
-vector<Entity*> playerBullets;
+Entity* playerBullet;
 vector<Entity*> enemyBullets;
 GLuint spriteSheet;
 
@@ -76,12 +77,12 @@ struct Entity
 {
 	Entity() {}
 	Entity(const GLuint& texture, float u, float v, float width, float height, float size, Vector position, float xScale, float yScale, float angle) 
-		: textureImage(texture), u(u / 256.0), v(v / 256.0), width(width / 256.0), height(height / 256.0), size(size), position(position), 
+		: textureImage(texture), u(u / 4096.0), v(v / 4096.0), width(width / 4096.0), height(height / 4096.0), size(size), position(position), 
 			xScale(xScale), yScale(yScale), angle(angle)
 	{
-		modelMatrix.Translate(position.x, position.y, position.z);
+		/*modelMatrix.Translate(position.x, position.y, position.z);
 		modelMatrix.Scale(xScale, yScale, 1.0);
-		modelMatrix.Rotate(angle * (3.14159265 / 180.0));
+		modelMatrix.Rotate(angle * (3.14159265 / 180.0));*/
 		halfLengths = Vector(size * (width / height) * .5, size * .5, 0);
 		points.push_back(Vector(-halfLengths.x, -halfLengths.y, 0) * modelMatrix);
 		points.push_back(Vector(halfLengths.x, -halfLengths.y, 0) * modelMatrix);
@@ -93,6 +94,7 @@ struct Entity
 	{
 		if (alive)
 		{
+			modelMatrix.Identity();
 			modelMatrix.Translate(position.x, position.y, position.z);
 			modelMatrix.Scale(xScale, yScale, 1.0);
 			modelMatrix.Rotate(angle * (3.14159265 / 180.0));
@@ -137,7 +139,7 @@ struct Entity
 	}
 
 	virtual void update(float elapsed) {}
-	bool alive = true;
+	bool alive = false;
 	Matrix modelMatrix;
 	Vector position;
 	GLuint textureImage;
@@ -151,21 +153,61 @@ struct Entity
 	float angle;
 	std::vector<Vector> points;
 	Vector halfLengths;
-	float xSpeed = 2.0;
-	float ySpeed = 4.0;
 	bool isEnemy = false;
 };
 
 struct Bullet : public Entity
 {
-	Bullet(const GLuint& texture, float u, float v, float width, float height, float size, Vector position, float xScale, float yScale, float angle, bool moveLeft, bool moveUp)
-		: Entity(texture, u, v, width, height, size, position, xScale, yScale, angle) {}
+	Bullet(const GLuint& texture, float u, float v, float width, float height, float size, Vector position, float xScale, float yScale, float angle)
+		: Entity(texture, u, v, width, height, size, position, xScale, yScale, angle) {
+		alive = false;
+	}
+
+	void collisions(Entity* player)
+	{
+		std::pair<float, float> penetration;
+
+		std::vector<std::pair<float, float>> playerPoints;
+		std::vector<std::pair<float, float>> bulletPoints;
+
+		for (int i = 0; i < player->points.size(); i++) {
+			Vector point = player->points[i] * player->modelMatrix;
+			playerPoints.push_back(std::make_pair(point.x, point.y));
+		}
+
+		for (int i = 0; i < points.size(); i++) {
+			Vector point = points[i] * modelMatrix;
+			bulletPoints.push_back(std::make_pair(point.x, point.y));
+		}
+
+		bool collided = CheckSATCollision(playerPoints, bulletPoints, penetration);
+		if (collided)
+		{
+			//player loses
+			return;
+		}
+	}
 };
 
 struct Star : public Entity
 {
-	Star(const GLuint& texture, float u, float v, float width, float height, float size, Vector position, float xScale, float yScale, float angle, bool moveLeft, bool moveUp)
-		: Entity(texture, u, v, width, height, size, position, xScale, yScale, angle) {}
+	Star(const GLuint& texture, float u, float v, float width, float height, float size, Vector position, float xScale, float yScale, float angle)
+		: Entity(texture, u, v, width, height, size, position, xScale, yScale, angle) {
+		alive = true;
+	}
+	void update(float elapsed)
+	{
+		if (alive)
+		{
+			position.x -= elapsed;
+			if (position.x <= -17.75)
+			{
+				position.x = 17.75;
+				return;
+			}
+		}
+	}
+
 };
 
 struct Player : public Entity
@@ -176,20 +218,15 @@ struct Player : public Entity
 
 struct AI : public Entity
 {
-	AI(const GLuint& texture, float u, float v, float width, float height, float size, Vector position, float xScale, float yScale, float angle, bool moveLeft, bool moveUp)
+	AI(const GLuint& texture, float u, float v, float width, float height, float size, Vector position, float xScale, float yScale, float angle)
 		: Entity(texture, u, v, width, height, size, position, xScale, yScale, angle) {}
 };
 
 struct Enemy : public Entity
 {
-	Enemy(const GLuint& texture, float u, float v, float width, float height, float size, Vector position, float xScale, float yScale, float angle, bool moveLeft, bool moveUp)
+	Enemy(const GLuint& texture, float u, float v, float width, float height, float size, Vector position, float xScale, float yScale, float angle)
 		: Entity(texture, u, v, width, height, size, position, xScale, yScale, angle) {
 		isEnemy = true;
-	}
-
-	void update(float elapsed)
-	{
-		return;
 	}
 	void draw()
 	{
@@ -222,22 +259,15 @@ void collisions(Entity* entity1, Entity* entity2)
 	bool collided = CheckSATCollision(e1Points, e2Points, penetration);
 	if (collided)
 	{
-		entity1->position.x += (penetration.first * 0.5f);
-		entity1->position.y += (penetration.second * 0.5f);
-
-		entity2->position.x -= (penetration.first * 0.5f);
-		entity2->position.y -= (penetration.second * 0.5f);
-		if (entity1 != entities[0] && entity2 != entities[0])
+		if (entity1 == entities[0])
 		{
-			entity1->xSpeed *= -1.1;
-			entity1->ySpeed *= -1.1;
-			entity2->xSpeed *= -1.1;
-			entity2->ySpeed *= -1.1;
+			entity1->position.x += (penetration.first * 0.5f);
+			entity1->position.y += (penetration.second * 0.5f);
 		}
-		
-		if (entity1 == entities[0] || entity2 == entities[0])
+		else if (entity2 == entities[0])
 		{
-			done = true;
+			entity2->position.x -= (penetration.first * 0.5f);
+			entity2->position.y -= (penetration.second * 0.5f);
 		}
 	}
 }
@@ -262,6 +292,35 @@ void Setup()
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 	spriteSheet = LoadTexture(RESOURCE_FOLDER"sprites.png");
+
+
+	Vector p1Vec = Vector(-10.0, 0.0, 0.0);
+	Vector e1Vec = Vector(0.0, 0.0, 0.0);
+	Player* p1 = new Player(spriteSheet, 887.0, 3734.0, 241.0, 159.0, 1.0, p1Vec, .75, .75, 0.0);
+	p1->alive = true;
+	entities.push_back(p1);
+	//Enemy(const GLuint& texture, float u, float v, float width, float height, float size, Vector position, float xScale, float yScale, float angle)
+	Enemy* e1 = new Enemy(spriteSheet, 887.0, 3570.0, 162.0, 173.0, 1.0, e1Vec, 10.0, 10.0, 0.0);
+	e1->alive = true;
+	entities.push_back(e1);
+	//create one player bullet
+	//height = "855" width = "1403" y = "2398" x = "0"
+	playerBullet = new Bullet(spriteSheet, 0.0, 2398.0, 1403.0, 855.0, 1.0, p1Vec, 10.0, 10.0, 0.0);
+	float y = 12.0;
+	float x = -14.0;
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			//height = "92" width = "90" y = "3992" x = "465" name = "star.png" / >
+			Vector starVec = Vector(x, y, 0.0);
+			Star* star = new Star(spriteSheet, 465.0, 3992.0, 90.0, 92.0, 1.0, starVec, .1, .1, 0.0);
+			x += 3;
+			starsVec.push_back(star);
+		}
+		y -= 3;
+		x = -14.0;
+	}
 }
 
 void ProcessEvents(float elapsed)
@@ -272,19 +331,19 @@ void ProcessEvents(float elapsed)
 			done = true;
 		}
 	}
-	if (keys[SDL_SCANCODE_W] && entities[0]->position.y + entities[0]->halfLengths.y <= 2.0 * 2)
+	if (keys[SDL_SCANCODE_W] && entities[0]->position.y + entities[0]->halfLengths.y <= 2.0 * 5)
 	{
-		entities[0]->position.y += elapsed * 5.0;
+		entities[0]->position.y += elapsed * 5.0; 
 	}
-	if (keys[SDL_SCANCODE_A] && entities[0]->position.x - entities[0]->halfLengths.x >= -3.55 * 2)
+	if (keys[SDL_SCANCODE_A] && entities[0]->position.x - entities[0]->halfLengths.x >= -10.0)
 	{
 		entities[0]->position.x -= elapsed * 5.0;
 	}
-	if (keys[SDL_SCANCODE_S] && entities[0]->position.y - entities[0]->halfLengths.y >= -2.0 * 2)
+	if (keys[SDL_SCANCODE_S] && entities[0]->position.y - entities[0]->halfLengths.y >= -2.0 * 5)
 	{
 		entities[0]->position.y -= elapsed * 5.0;
 	}
-	if (keys[SDL_SCANCODE_D] && entities[0]->position.x + entities[0]->halfLengths.y <= 3.55 * 2)
+	if (keys[SDL_SCANCODE_D] && entities[0]->position.x + entities[0]->halfLengths.y <= 10.0)
 	{
 		entities[0]->position.x += elapsed * 5.0;
 	}
@@ -297,43 +356,48 @@ void ProcessEvents(float elapsed)
 		entities[0]->angle -= elapsed * 100.0;
 	}
 }
-/*
-std::vector<Entity*> entities;
-vector<Entity*> starsVec;
-vector<Entity*> playerBullets;
-vector<Entity*> enemyBullets;
-vector<Entity*> AI;
-*/
 
 void Update(float elapsed)
 {
-	for (Entity* curr : entities)
+	for (size_t i = 0; i < entities.size(); i++)
 	{
-		curr->update(elapsed);
-		for (Entity* curr2 : entities)
+		entities[i]->update(elapsed);
+		for (size_t j = i; j < entities.size(); j++)
 		{
-			if (curr != curr2)
+			if (i != j)
 			{
-				collisions(curr, curr2);
+				collisions(entities[i], entities[j]);
 			}
 		}
 	}
+	viewMatrix.Identity();
+	viewMatrix.Translate(-entities[0]->position.x, -entities[0]->position.y, 0);
+	textured.SetViewMatrix(viewMatrix);
+	for (Entity* star : starsVec)
+	{
+		star->update(elapsed);
+	}
+	playerBullet->update(elapsed);
+	for (Entity* bullet : enemyBullets)
+	{
+		bullet->update(elapsed);
+	}
 }
-/*
-std::vector<Entity*> entities;
-vector<Entity*> starsVec;
-vector<Entity*> playerBullets;
-vector<Entity*> enemyBullets;
-*/
+
 void Render()
 {
+	for (Entity* curr : starsVec)
+	{
+		curr->draw();
+	}
 	for (Entity* curr : entities)
 	{
 		curr->draw();
 	}
-	for (Entity* curr : starsVec)
+	if (playerBullet->alive) { playerBullet->draw(); }
+	for (Entity* bullet : enemyBullets)
 	{
-		curr->draw();
+		bullet->draw();
 	}
 }
 
